5ea1732dc90511cfd254f5a6cfb2c756c346ad69
lramos <laisllaramos@gmail.com>
2020-08-23 22:45:48 -0400

    added image gallery to shelter and pets pages

--- src/pages/PetView/index.tsx
+++ src/pages/PetView/index.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import Layout from "../../components/Layout";
 import "./index.css";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { Link }  from "react-router-dom";
+import ImageGallery from "react-image-gallery";
+
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
 
 const PetView: React.FC = () => {
   return (
@@ -10,10 +27,101 @@ const PetView: React.FC = () => {
         <article>
           <div className="article-header">
             <div className="article-title">
-              <h1>Pet View</h1>
+              <h1>Ginger</h1>
             </div>
           </div>
-          <div></div>
+          <Card>
+            <Card.Body>
+              <Row>
+                <Col lg={6}><h5>Story</h5>
+                  <Card.Text>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Quisque accumsan tellus id lacus rutrum auctor. Nam et
+                      eros ac enim ultricies cursus. Duis eget laoreet nisl.
+                      Nunc viverra augue eros, quis aliquet odio congue in.
+                      Phasellus cursus tortor a tincidunt fermentum. Aenean ac
+                      risus porttitor, placerat ante eget, blandit dui. Integer
+                      sapien libero, maximus et laoreet ac, imperdiet tristique
+                      nibh. Fusce dictum ante tellus, quis finibus diam dictum
+                      id. Pellentesque malesuada ligula a urna vestibulum, ut
+                      eleifend urna luctus. Curabitur a leo ultricies,
+                      sollicitudin eros nec, porta ante.{" "}
+                    </p>
+                  </Card.Text>
+                  <Row>
+                    <Col>
+                      <h5>Breed</h5>
+                      <Card.Text>Shitzu</Card.Text></Col>
+                    <Col>
+                      <h5>Gender</h5>
+                      <Card.Text>Female</Card.Text>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <h5>Size</h5>
+                      <Card.Text>Small</Card.Text>
+                    </Col>
+                    <Col>
+                      <h5>Weight</h5>
+                      <Card.Text>3kg</Card.Text>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                  <h5>Age</h5>
+                  <Card.Text>Adult</Card.Text>
+                    </Col>
+                    <Col>
+                  <h5>Hair</h5>
+                  <Card.Text>Long hair</Card.Text>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                  <h5>Special needs?</h5>
+                  <Card.Text>No</Card.Text>
+                    </Col>
+                    <Col>
+                  <h5>Neuter?</h5>
+                  <Card.Text>No</Card.Text>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                  <h5>Vaccine?</h5>
+                  <Card.Text>No</Card.Text>
+                    </Col>
+                    <Col>
+                  <h5>Chip?</h5>
+                  <Card.Text>No</Card.Text>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                  <h5>Leash?</h5>
+                  <Card.Text>No</Card.Text>
+                    </Col>
+                  </Row>
+                  
+                </Col>
+                <Col lg={6} className="text-center">
+                  <ImageGallery items={images} />
+                  <Row>
+                    <Col>
+      <Link className="btn" to="/">
+        Contact
+      </Link>
+      <Link className="btn" to="/">
+        Share
+      </Link>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
         </article>
       </Container>
     </Layout>
--- src/pages/ShelterView/index.tsx
+++ src/pages/ShelterView/index.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import Layout from "../../components/Layout";
 import "./index.css";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { Link }  from "react-router-dom";
+import ImageGallery from "react-image-gallery";
+
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
 
 const ShelterView: React.FC = () => {
   return (
@@ -10,10 +27,67 @@ const ShelterView: React.FC = () => {
         <article>
           <div className="article-header">
             <div className="article-title">
-              <h1>Shelter View</h1>
+              <h1>Bee Animal Shelter</h1>
             </div>
           </div>
-          <div></div>
+          <Card>
+            <Card.Body>
+              <Row>
+                <Col lg={6}><h5>Story</h5>
+                  <Card.Text>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Quisque accumsan tellus id lacus rutrum auctor. Nam et
+                      eros ac enim ultricies cursus. Duis eget laoreet nisl.
+                      Nunc viverra augue eros, quis aliquet odio congue in.
+                      Phasellus cursus tortor a tincidunt fermentum. Aenean ac
+                      risus porttitor, placerat ante eget, blandit dui. Integer
+                      sapien libero, maximus et laoreet ac, imperdiet tristique
+                      nibh. Fusce dictum ante tellus, quis finibus diam dictum
+                      id. Pellentesque malesuada ligula a urna vestibulum, ut
+                      eleifend urna luctus. Curabitur a leo ultricies,
+                      sollicitudin eros nec, porta ante.{" "}
+                    </p>
+                  </Card.Text>
+                  <Row>
+                    <Col>
+                      <h5>Contact</h5>
+                      <Card.Text>James D. Box</Card.Text>
+                      <Card.Text>beeanimalshelter@gmail.com </Card.Text>
+                      <a href="">www.beeanimalshelter.com </a>
+                      </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+          <a href="www.petrescue.com" className="social-midia-icons">
+            <i className="fab fa-twitter-square fa-3x"></i>
+          </a>
+          <a href="www.petrescue.com" className="social-midia-icons">
+            <i className="fab fa-google-plus-square fa-3x"></i>
+          </a>
+          <a href="www.petrescue.com" className="social-midia-icons">
+            <i className="fab fa-facebook-square fa-3x"></i>
+          </a>
+                      </Col>
+                  </Row>
+                  
+                </Col>
+                <Col lg={6} className="text-center">
+                  <ImageGallery items={images} />
+                  <Row>
+                    <Col>
+      <Link className="btn" to="/pets">
+        Pets
+      </Link>
+      <Link className="btn" to="/">
+        Donation
+      </Link>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
         </article>
       </Container>
     </Layout>
